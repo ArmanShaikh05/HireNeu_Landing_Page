4963fbe3c352e4ed19e7c794b25dc36bb0106dcd
Arkodeep Chatterjee <arkodeep3404@gmail.com>
2024-07-01 23:46:18 +0530

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,212 @@
+import {
+  people01,
+  people02,
+  people03,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  microsoft,
+  aws,
+  chatgpt,
+  langchain,
+  send,
+  shield,
+  star,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Discovery",
+    content:
+      "Dive into company profiles featuring culture, testimonials, and employee benefits to find your perfect fit.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Development",
+    content:
+      "Experience job hunting with engaging, video-based job postings directly from recruiters.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Delivery",
+    content:
+      "Create a compelling video portfolio that highlights your skills, projects, and professional journey.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "NexaWings made my SAAS idea a reality with GoCPT! This AI powered software will revolutionize healthcare billing.",
+    name: "Tiffinne Perkins",
+    title: "Medical Professional",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Storyia has been brought to life in an exceptional manner by NexaWings. They totally exceeded all our expectations.",
+    name: "Fehym Evo",
+    title: "Entrepreneur",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "NexaWings played a pivotal role in transforming my restaurant operations. Their expertise in implementing AI chatbots significantly improved my customer operations.",
+    name: "Rory Russel",
+    title: "Restaraunt Owner",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Hours Saved",
+    value: "1700+",
+  },
+  {
+    id: "stats-2",
+    title: "Clients Served",
+    value: "40+",
+  },
+  {
+    id: "stats-3",
+    title: "Saved in Operations",
+    value: "$70k+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: microsoft,
+  },
+  {
+    id: "client-2",
+    logo: aws,
+  },
+  {
+    id: "client-3",
+    logo: chatgpt,
+  },
+  {
+    id: "client-4",
+    logo: langchain,
+  },
+];
